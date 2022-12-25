ed684dfe29b3165dd00d0f1b5035bc3ee535fa60
Jorgos Papapostolu <JorgoPapapostolu@gmail.com>
2022-12-25 01:24:09 +0100

    init

---
+++ DayOne/main.js
@@ -0,0 +1,20 @@
+import { readFileSync } from "node:fs";
+
+const elfs = readFileSync("irgendwie.txt", { encoding: "utf-8" })
+  .replace(/\r/g, "")
+  .trim()
+  .split("\n\n");
+
+// funktion
+function elfchen() {
+  const calories = elfs.map((elf) => {
+    const caloriesTwo = elf.split("\n").map(Number);
+    return caloriesTwo.reduce((a, b) => a + b, 0);
+  });
+console.log(Math.max(...calories));
+}
+// funktion ende
+
+
+//aufruf
+elfchen();
